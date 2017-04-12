af2e21bb9061e83ae6ac4e322f1a09a82faae3d9
Fernando J Ortiz Sacarello <forzzak@gmail.com>
2017-04-12 17:08:52 -0400

    Lexer, Parser, Intermediate code and compiler : DONE
    TODO:
    TEST

--- ChromeItApplication.py
+++ ChromeItApplication.py
@@ -1,6 +1,7 @@
 import chromeItParser as parser
 
 import subprocess
+import os
 
 
 file = "ChromeItCode.txt"
@@ -8,11 +9,13 @@ file = "ChromeItCode.txt"
 parser.translateCode(file)
 
 
+devToolPath = open("setting1.txt").read()
+cmds = ['C:', "cd ../", 'VsDevCmd.bat', os.getcwd()[:2], 'cd chromeIt/chromeitcompilable','cl /EHsc /MD RazerChromaApplication.cpp ChromaSDKImpl.cpp']
+#cmds = [devToolPath[:2], "cd " +devToolPath[3:] , 'VsDevCmd.bat', os.getcwd()[:2], 'cd chromeitcompilable','cl /EHsc /MD RazerChromaApplication.cpp ChromaSDKImpl.cpp']
 
-cmds = ['G:', 'cd Visual Studio Enterprise\Common7\Tools', 'VsDevCmd.bat', 'C:', 'cd chromeitcompilable','cl /EHsc /MD RazerChromaApplication.cpp ChromaSDKImpl.cpp']
 encoding = 'latin1'
-p = subprocess.Popen('cmd.exe', stdin=subprocess.PIPE,
-             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+p = subprocess.Popen('cmd.exe', stdin=subprocess.PIPE,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 for cmd in cmds:
     p.stdin.write(cmd + "\n")
 p.stdin.close()
